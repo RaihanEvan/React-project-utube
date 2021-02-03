7f9147472649c3e174c2f4d229e111611fb5e8c2
RaihanEvan <raihan.evan@gmail.com>
2021-02-03 15:12:31 +0600

    components added

---
+++ src/components/SearchBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+class SearchBar extends React.Component{
+    state = {
+        searchTerm:'',
+    }
+    render(){
+        return(
+            <h1>This is a search component</h1>
+        )
+    }
+}
+
+export default SearchBar;
---
+++ src/components/VideoList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const VideoList = ()=>{
+    return(
+        <h1>This is a video list</h1>
+    )
+}
+
+export default VideoList;
